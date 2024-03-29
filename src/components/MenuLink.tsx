2651206c4302cf0253a55f563befd3e852fd7080
zhanghaojie <1455123087@qq.com>
2024-03-29 11:49:46 +0800

    Refactor MenuLink component styling

--- src/components/MenuLink.tsx
+++ src/components/MenuLink.tsx
@@ -2,12 +2,13 @@ import Link from 'next/link';
 import React from 'react';
 
 export function MenuLink({
-  href, children,
-}: React.PropsWithChildren<{ href: string; }>) {
+  href,
+  children,
+}: React.PropsWithChildren<{ href: string }>) {
   return (
     <Link
       href={href}
-      className="hover:text-black text-gray-500 text-lg pe-2 no-underline"
+      className="hover:text-black text-gray-500 text-lg pe-2 no-underline hover:underline"
     >
       {children}
     </Link>
