ea078045aac16cd3d646474c2eea6a575663e526
zhanghaojie <1455123087@qq.com>
2024-03-30 22:58:13 +0800

    Refactor header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,15 +11,13 @@ export const Header: React.FC<Props> = () => {
     <div className="bg-gray-50">
       <header className="content py-2">
         <div className="flex flex-row items-center">
-          <h3 className="p-0 m-0 font-serif">
+          <h1 className="m-0 font-serif">
             <HomeLink>Welcome</HomeLink>
-          </h3>
-          <div className="block ms-auto">
-            <nav className="flex flex-row gap-2 font-serif font-bold">
-              <MenuLink href="/posts">Posts</MenuLink>
-              <MenuLink href="/notes">Notes</MenuLink>
-            </nav>
-          </div>
+          </h1>
+          <nav className="flex flex-row gap-2 font-serif font-bold ms-auto">
+            <MenuLink href="/posts">Posts</MenuLink>
+            <MenuLink href="/notes">Notes</MenuLink>
+          </nav>
         </div>
       </header>
     </div>
